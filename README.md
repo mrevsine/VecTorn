c8a7863ca41e451d782edefeeb728ab584a848e8
Mahler Revsine <mrevsine@gmail.com>
2023-12-02 16:58:30 -0500

    Adding code and results of full recombinant vector simulation

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # VecTorn
 Program to identify vector contamination in genomic reads
+
+## Methods
+
+### Data simulation
+10 genes were identified from human Chr22:23M-24M of the GRCH38 release: BCR, DDT, DRICH1, GNAZ, GSTTB2, IGLL1, RAB36, SLC2A11, SMARCB1, and ZNF70. For each, we downloaded their mRNA sequence from NCBI, then simulated converting it to its corresponding CDNA sequence by removing any polyA tails. 10 vectors with an annotated Multiple Cloning Site (MCS) were chosen from the UniVec database: AF113968.2, D63844.1, DQ845760.1, EU186085.1, GU733833.1, JN581376.1, JN900237.1, JX025642.1, U13872.1, and X65329.1. We simulated creating a recombinant sequence from each pair of 10 genes and 10 vectors by inserting the gene's CDNA sequence into the middle of the vector's MCS. For each recombinant sequence we then simulated creating a single SNP within its insert gene region 2 times, for a total of 200 artificial recombinant vectors, each with a single unique mutation.
--- src/full_simulation.py
+++ src/full_simulation.py
@@ -1,8 +1,14 @@
 # Code to produce simulated reads of vector inserts
 
+
+###=============================================================================
+# Imports
+
 import math
 import numpy as np
+import os
 import random
+
 
 ###=============================================================================
 # Functions
@@ -41,48 +47,56 @@ def write_fasta(names, seqs, out_fa_file, max_char_per_line=80):
         f.write(seq[i:i+max_char_per_line] + "\n")
 
 
-# create random SNP in a genomic sequence
-# choose a random locus and a random base to replace it with
-def create_random_SNP(seq, seed=1):
-  random_locus = random.randrange(len(seq))
-  bases = ["A","C","G","T"]
-  new_bases = [base for base in bases if base != seq[random_locus]]
-  random_base = new_bases[random.randrange(3)]
-  return seq[:random_locus] + random_base + seq[random_locus+1:]
+# Remove a polyA tail from an input sequence, if it exists
+# Return cleaned sequence
+# 4 A's would randomly occur ~ 0.25% of the time, seems like a good threshold 
+def remove_polyA_tail(seq, min_tail_length=4):
+  ending_A_count = 0
+  for c in seq[::-1]:
+    if c.upper() == "A":
+      ending_A_count += 1
+    else:
+      break
+  if ending_A_count < min_tail_length:
+    return seq
+  return seq[:-ending_A_count]
 
 
-# select a vector among options
-def choose_vectors(vector_options, n_samples=1, seed=1):
+# Create a random SNP in a sequence
+# Assume seed has been set previously
+def create_random_SNP(seq):
+  SNP_locus = random.randrange(len(seq))
+  seq_locus_base = seq[SNP_locus].upper()
+  SNP_base_options = [c for c in ["A","C","G","T"] if c != seq_locus_base]
+  SNP_new_base = SNP_base_options[random.randrange(3)]
+  SNP_seq = seq[:SNP_locus] + SNP_new_base + seq[SNP_locus+1:]
+  return SNP_locus, SNP_new_base, SNP_seq
+  
+
+# Create a random SNP in each sequence from a list
+def create_random_SNPs(seqs, seed=1):
   random.seed(seed)
-  return random.choices(vector_options, k=n_samples)
+  SNP_loci = [random.randrange(len(seq)) for seq in seqs]
+  SNP_bases = [random.randrange(3) for _ in range(len(seqs))]
+  SNP_seqs = []
+  for i in range(len(seqs)):
+    seq = seqs[i]
+    SNP_locus = SNP_loci[i]
+    seq_locus_base = seq[SNP_locus].upper()
+    SNP_base_options = [c for c in ["A","C","G","T"] if c != seq_locus_base]
+    SNP_new_base = SNP_base_options[SNP_bases[i]]
+    SNP_seqs.append(seq[:SNP_locus] + SNP_new_base + seq[SNP_locus+1:])
+  return SNP_seqs
 
 
 # choose locus within a vector Multiple Cloning Site at which to split
 # for now just choose the midpoint
 # in the future, we may randomize this selection process
 def get_split_index(start_idx, end_idx, seed=1):
-  # seed(seed) # not needed yet
+  #random.seed(seed) # not needed yet
   range = end_idx - start_idx
   half_range = int(range // 2) # integer division is analagous to floor()
   return start_idx + half_range
-
-
-# select vector insertion details n_samples times
-# return list of tuples of (vector name, vector insertion position) for each sample
-def get_vector_insertion_params(vector_options, vector_positions, n_samples=1, all_same_vector=False, seed=1):
-  
-  # choose vectors and their split positions
-  if all_same_vector:
-    vector_choice = choose_vectors(vector_options, n_samples=1, seed=seed)[0]
-    vector_choices = [vector_choice for _ in range(n_samples)]
-  else:
-    vector_choices = choose_vectors(vector_options, n_samples=n_samples, seed=seed)
-
-  # for each vector, get insertion position
-  vector_inserts = [get_split_index(*vector_positions[vec], seed=seed) for vec in vector_choices]
-
-  # return tuple list of [(vector_i, split_index_i)]
-  return [(c,i) for c,i in zip(vector_choices, vector_inserts)]
 
 
 # create a vector insert sequence starting with vector seq, insert seq, and position at which to split the vector seq
@@ -90,45 +104,71 @@ def perform_insert(vector_seq, vector_split_pos, insert_seq):
   return vector_seq[:vector_split_pos] + insert_seq + vector_seq[vector_split_pos:]
 
 
-# create a vector insert sequence, starting with no needed parameters
-# uses rng to select several parameters
-def get_insertions(full_seq, vector_names, vector_seqs, vector_positions, n_samples=1, mean_insert_length=10000, sd_insert_length=None, all_same_vector=False, ref_name="ref", seed=1):
+# Starting with N genes and V vectors, create N*V recombinant sequences by inserting each gene into each vector
+# For each recombinant seq, create a SNP at a random position within the inserted gene
+def create_recombinant_sequences(gene_dict, vector_dict, vector_MCS_position_dict, n_mutation_sims_per_recomb=2, seed=1):
 
-  # generate sample insertion sequence params from full sequence
-  insert_seq_params = get_insert_seq_params(len(full_seq), \
-                                            mean_length=mean_insert_length, \
-                                            sd_length = sd_insert_length, \
-                                            n_samples=n_samples, \
-                                            seed=seed)
+  random.seed(seed)  
+  recomb_seqs = []
+  recomb_seq_names = []
+ 
+  # simulate for each pair of gene and vector
+  for n,(gene_name,gene_seq) in enumerate(gene_dict.items()):
+    for v,(vector_name,vector_seq) in enumerate(vector_dict.items()):
 
-  # generate vector insert data for each insertion sequence
-  vector_insert_params = get_vector_insertion_params(vector_options=vector_names, vector_positions=vector_positions, n_samples=n_samples, all_same_vector=all_same_vector, seed=seed)  
+      # Simulate `n_mutation_sims_per_recomb` times per gene,vector pair
+      # In Vecuum, this was 2 times
+      last_locus = -1
+      last_base = ""
+      for i in range(n_mutation_sims_per_recomb):
 
-  # get recombinant vector insertion sequences
-  recombinant_seqs = [perform_insert(vector_seqs[vec_name], split_idx, full_seq[insert_idx:insert_idx+insert_length]) for ((vec_name, split_idx), (insert_idx, insert_length)) in zip(vector_insert_params, insert_seq_params)]
+        # Make sure that all simulated mutations per gene,vector pair are unique
+        SNP_locus, SNP_base, SNP_gene = create_random_SNP(gene_seq)      
+        while (SNP_locus == last_locus) and (SNP_base == last_base):
+          SNP_locus, SNP_base, SNP_gene = create_random_SNP(gene_seq)      
+        last_locus = SNP_locus
+        last_base = SNP_base
 
-  # get recombinant insert names for the fasta headers
-  recombinant_names = [ref_name + ":" + str(insert_idx) + "-" + str(insert_idx+insert_length) + "_" + vec_name + ":" + str(split_idx) for ((vec_name, split_idx), (insert_idx, insert_length)) in zip(vector_insert_params, insert_seq_params)]
+        # Perform split
+        vec_split_idx = get_split_index(*vector_MCS_position_dict[vector_name])
+        recomb_seq = perform_insert(vector_seq, vec_split_idx, SNP_gene)
+        recomb_seqs.append(recomb_seq)
 
-  # return recombinant names and sequences
-  return recombinant_names, recombinant_seqs
+        # Name is of the form vectorName:vectorSplitIdx_geneName:geneLength_SNPLocus:OriginalBase:MutatedBase
+        recomb_seq_name = vector_name + ":" + str(vec_split_idx) + "_" + gene_name + ":" + str(len(SNP_gene)) + "_" + str(SNP_locus) + ":" + gene_seq[SNP_locus] + ":" + SNP_base
+        recomb_seq_names.append(recomb_seq_name) 
+
+  return recomb_seq_names, recomb_seqs
 
 
 ###=============================================================================# Read in needed data
 
 # needed files
+gene_dir = "data/genes/"
+gene_fa_files = [s for s in os.listdir(gene_dir) if s.endswith(".fa")]
 vector_dir = "data/vectors/"
 vector_pos_bed = vector_dir + "clone_vec.bed"
-reference_seq_fa_file = "data/ref/chr22_14M_15M.fa"
+reference_seq_fa_file = "data/ref/chr22_23M_24M.fa"
 
-# read vector position file
+# Read in gene fastas
+# Convert mRNA sequences to CDNA by removing polyA tail
+gene_CDNA_dict = {}
+for gene_fa_file in gene_fa_files:
+  gene_name = gene_fa_file.split(".")[0]
+  with open(gene_dir + gene_fa_file, "r") as f:
+    lines = f.readlines()
+    mrna_seq = "".join([line.rstrip() for line in lines[1:]])
+    cdna_seq = remove_polyA_tail(mrna_seq)
+    gene_CDNA_dict[gene_name] = cdna_seq
+
+# read vector Multiple Cloning Site position file
 with open(vector_pos_bed, "r") as f:
   lines = f.readlines()
-vector_positions = {}
+vector_MCS_positions = {}
 for line in lines[1:]:
   line_terms = line.rstrip().split("\t")
-  vector_positions[line_terms[0]] = (int(line_terms[1]), int(line_terms[2]))
-vectors = list(vector_positions.keys())
+  vector_MCS_positions[line_terms[0]] = (int(line_terms[1]), int(line_terms[2]))
+vectors = list(vector_MCS_positions.keys())
 
 # read vector sequence files
 vector_seqs = {}
@@ -139,103 +179,21 @@ for vector in vectors:
   vector_seqs[vector] = vector_seq
 
 
-###=============================================================================# Test functionality
+###============================================================================
+# Create recombinant vectors
 
-run_tests = False
+# For simplicity, use 10 genes and 10 vectors
+# Remove KP126802.1, the longest one
+del vector_MCS_positions["KP126802.1"]
+del vector_seqs["KP126802.1"]
+vectors = [v for v in vectors if v != "KP126802.1"]
 
-if run_tests:
+# Create recombinant sequences with mutations
+recomb_names, recomb_seqs = create_recombinant_sequences(gene_CDNA_dict, vector_seqs, vector_MCS_positions)
 
-  ## read_fasta(fa_file)
-  print("Testing read_fasta()")
-  ref_headers, ref_seqs = read_fasta(reference_seq_fa_file)
-  print("%s headers, %s seqs" % (len(ref_headers), len(ref_seqs)))
-  ref_header = ref_headers[0]
-  ref_seq = ref_seqs[0]
-  print(ref_header)
-  print(len(ref_seq))
-  print("--------------------")
+# Write recombinant seqs to file
+out_fa_file = "data/simulated_reads/recomb_vectors_b.fa"
+write_fasta(recomb_names, recomb_seqs, out_fa_file)
 
-  ## get_insert_seq_params(full_seq_length, mean_length=10000, sd_length=None, n_samples=1, seed=1):
-  print("Testing get_insert_seq_params()")
-  insert_seq_params = get_insert_seq_params(len(ref_seq), n_samples = 10)
-  print(insert_seq_params)
-  insert_seq_params = get_insert_seq_params(len(ref_seq), n_samples = 10)
-  print(insert_seq_params)
-  print("--------------------")
 
-  ## choose_vectors(vector_options, n_samples=1, seed=1):
-  print("Testing choose_vectors()")
-  vector_choices = choose_vectors(vectors, n_samples = 10)
-  print(vector_choices)
-  vector_choices = choose_vectors(vectors, n_samples = 10)
-  print(vector_choices)
-  print("--------------------")
-
-  ## get_split_index(start_idx, end_idx, seed=1):
-  print("Testing get_split_index()")
-  for k,v in vector_positions.items():
-    print("%s: split of %s and %s = %s" % (k, v[0], v[1], get_split_index(v[0],v[1])))
-  print("--------------------")
-
-  ## get_vector_insertion_params(vector_options, vector_positions, n_samples=1, all_same_vector=False, seed=1):
-  print("Testing get_vector_insertion_params()")
-  vector_insertion_params = get_vector_insertion_params(vectors, vector_positions, n_samples = 10, all_same_vector = False)
-  print(vector_insertion_params)
-  vector_insertion_params = get_vector_insertion_params(vectors, vector_positions, n_samples = 10, all_same_vector = False)
-  print(vector_insertion_params)
-  vector_insertion_params = get_vector_insertion_params(vectors, vector_positions, n_samples = 10, all_same_vector = True)
-  print(vector_insertion_params)
-  print("--------------------")
-
-  ## get_insertions(full_seq, ref_name="ref", vector_names, vector_seqs, vector_positions, n_samples=1, mean_insert_length=10000, sd_insert_length=None, all_same_vector=False, seed=1):
-  print("Testing perform_insert(), get_insertions(), and write_fasta()")
-  recomb_names, recomb_seqs = get_insertions(full_seq = ref_seq, \
-                                             vector_names = vectors,
-                                             vector_seqs = vector_seqs,
-                                             vector_positions =vector_positions, \
-                                             n_samples = 10,
-                                             seed = 1)
-  print("Writing fasta")
-  results_fa_file = "results/test_results_1.fa"
-  write_fasta(recomb_names, recomb_seqs, results_fa_file)
-
-  # Now read it back in and check that all data matches
-  print("Testing fasta")
-  in_recomb_names, in_recomb_seqs = read_fasta(results_fa_file)
-  print("Do headers match?", recomb_names == in_recomb_names)
-  print("Do sequences match?", recomb_seqs == in_recomb_seqs)
-  for header,seq in zip(in_recomb_names, in_recomb_seqs):
-    ref_start = int(header.split("_")[0].split(":")[1].split("-")[0])
-    ref_end = int(header.split("_")[0].split(":")[1].split("-")[1])
-    ref_seq_segment = ref_seq[ref_start:ref_end]
-    vector_name = header.split("_")[1].split(":")[0]
-    vector_split = int(header.split("_")[1].split(":")[1])
-    vector_seq_1 = vector_seqs[vector_name][:vector_split]
-    vector_seq_2 = vector_seqs[vector_name][vector_split:]
-    full_check_seq = vector_seq_1 + ref_seq_segment + vector_seq_2
-    print("Does " + header + " seq match?", full_check_seq == seq)
-    if full_check_seq != seq:
-      print("Expected length is %s but observed length is %s" % (len(seq), len(full_check_seq)))
-  print("--------------------")
-
-  print("All tests done")
-
-###=============================================================================
-# Use script
-
-ref_headers, ref_seqs = read_fasta(reference_seq_fa_file)
-ref_seq = ref_seqs[0]
-run_seed = 123
-run_n_samples = 100
-run_insert_length = 10000
-run_out_fa_file = "data/simulated_reads/100_samples_10k_length_123_seed_b.fa"
-#run_out_fa_file = "results/" + str(run_n_samples) + "samples_" + str(run_seed) + "seed.fa"
-recomb_names, recomb_seqs = get_insertions(full_seq = ref_seq, \
-                                           vector_names = vectors,
-                                           vector_seqs = vector_seqs,
-                                           vector_positions =vector_positions, \
-                                           n_samples = run_n_samples,
-                                           mean_insert_length = run_insert_length, \
-                                           seed = run_seed)
-write_fasta(recomb_names, recomb_seqs, run_out_fa_file)
 
