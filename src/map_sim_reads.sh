ba1e32c1f8b2116a9d2068aca1edeae7e379fb3a
Adele Valeria <avaleri4@jhu.edu>
2023-12-08 20:50:54 -0500

    Update map_sim_reads.sh
    change index dir

--- src/map_sim_reads.sh
+++ src/map_sim_reads.sh
@@ -5,7 +5,7 @@ fq1=${sim_path}/reads.1.fastq
 fq2=${sim_path}/reads.2.fastq
 ref_name=chr22_23M_24M
 ref_fa=data/ref/${ref_name}.fa
-ref_index_folder=data/ref/index
+ref_index_folder=data/ref/bowtieidx
 ref_index=${ref_index_folder}/${ref_name}
 
 if [ ! -d $ref_index_folder ]; then
